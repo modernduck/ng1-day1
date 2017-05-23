9f56efc90e60877ad7aae0a40c57093914b19506
Sompop Kulapalanont <sompop.kulapalanont@gmail.com>
2017-05-23 09:13:44 +0700

    add users in hello

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
             .controller("HelloController", function(){
                 var hello = this;
                 hello.name = " Hi!";
+                hello.users = [
+                    {"picture":"http://placehold.it/100x100", "name":"Sompop", "age":29 },
+                    {"picture":"http://placehold.it/100x100", "name":"Zulu", "age":18 },
+                    {"picture":"http://placehold.it/100x100", "name":"Bazoo", "age":67 },
+                    {"picture":"http://placehold.it/100x100", "name":"Mocca", "age":39 },
+                    {"picture":"http://placehold.it/100x100", "name":"Angular", "age":45 }
+                ]
 
                 /*hello.sayHi = function(){
 
